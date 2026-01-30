1079d8ab8204ea1c9b722093c9ca926ef2631a03
mahfujufo <168577775+mahfujufo@users.noreply.github.com>
2026-01-30 16:51:53 +0600

    hover and offline service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/* 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("my-cache").then(cache => {
@@ -13,6 +14,50 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+*/
+
+const CACHE_NAME = "academic-words-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./words.json",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch (offline-first)
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
